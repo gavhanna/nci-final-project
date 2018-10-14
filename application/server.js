8256408f0401de9d80fd57aad0040319bf02d5ac
Gav Hanna <gavhanna@gmail.com>
2018-10-14 20:29:07 +0100

    registered JWT strategy with passport

--- application/server.js
+++ application/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require("./routes/api/users");
 
@@ -18,6 +19,10 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
+
+app.use(passport.initialize());
+// Passport Config
+require("./config/passport")(passport);
 
 // User Routes
 app.use("/api/users", users);
