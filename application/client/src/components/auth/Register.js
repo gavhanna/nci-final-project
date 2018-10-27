3fbd697acab9671b7995cc7935e671c7eb5315ce
Gav Hanna <gavhanna@gmail.com>
2018-10-27 17:52:35 +0100

    state and submit set up, not connected to backend yet

--- application/client/src/components/auth/Register.js
+++ application/client/src/components/auth/Register.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react'
+import classnames from "classnames";
 
 class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      username: "",
+      email: "",
+      blurb: "",
+      password: "",
+      password2: "",
+      errors: {}
+    }
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    }
+    console.log(newUser);
+
+    //this.props.registerUser(newUser, this.props.history);
+  }
+
   render() {
+    const { errors } = this.state;
     return (
       <React.Fragment>
         <div className="container-fill-height landing-page">
@@ -10,24 +42,56 @@ class Register extends Component {
             <p>Please enter your details</p>
             <div className="row">
               <div className="col-sm-8 col-md-3 col-xs-10 m-auto">
-                <form>
+                <form noValidate onSubmit={this.onSubmit}>
                   <div className="form-group">
-                    <label for="exampleInputEmail1">Name</label>
-                    <input name="name" type="text" className="form-control" id="name" aria-describedby="nameHelp" placeholder="Enter name" />
+                    <label>Name</label>
+                    <input
+                      name="name"
+                      type="text"
+                      className={classnames("form-control form-control-lg", {
+                        "is-invalid": errors.name
+                      })}
+                      id="name"
+                      placeholder="Enter name"
+                      value={this.state.name}
+                      onChange={this.onChange} />
                   </div>
                   <div className="form-group">
-                    <label for="email">Email address</label>
-                    <input name="email" type="email" className="form-control" id="email" aria-describedby="emailHelp"
-                      placeholder="Enter email" />
-                    <small id="emailHelp" className="form-text text-light">We'll never share your email with anyone else.</small>
+                    <label>Email address</label>
+                    <input
+                      name="email"
+                      type="email"
+                      className={classnames("form-control form-control-lg", {
+                        "is-invalid": errors.email
+                      })}
+                      id="email"
+                      placeholder="example@email.com"
+                      value={this.state.email}
+                      onChange={this.onChange} />
                   </div>
                   <div className="form-group">
-                    <label for="password">Password</label>
-                    <input name="password" type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    <label>Password</label>
+                    <input
+                      name="password"
+                      type="password"
+                      className={classnames("form-control form-control-lg", {
+                        "is-invalid": errors.password
+                      })}
+                      id="password"
+                      value={this.state.password}
+                      onChange={this.onChange} />
                   </div>
                   <div className="form-group">
-                    <label for="password2">Confirm Password</label>
-                    <input name="password2" type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    <label>Confirm Password</label>
+                    <input
+                      name="password2"
+                      type="password"
+                      className={classnames("form-control form-control-lg", {
+                        "is-invalid": errors.password2
+                      })}
+                      id="password2"
+                      value={this.state.password2}
+                      onChange={this.onChange} />
                   </div>
                   <button type="submit" className="btn btn-lg btn-pill btn-info">Submit</button>
                 </form>
