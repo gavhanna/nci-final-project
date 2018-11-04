0d6f90458112391908524b95e6148c2f96f15ea8
Gav Hanna <gavhanna@gmail.com>
2018-11-04 18:07:28 +0000

    follow profile section working

--- application/client/src/components/user/profile/following/FollowCard.js
+++ application/client/src/components/user/profile/following/FollowCard.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom"
 
 export default class FollowCard extends Component {
   render() {
+    console.log(this.props);
     return (
-      <div className="media w-100">
-        <img className="media-object rounded-circle mr-3" src="https://fillmurray.com/301/300" />
-        <div className="media-body align-self-center">
-          <button className="btn btn-outline-primary btn-sm float-right">
-            <span className="icon icon-add-user"></span> Follow
-                  </button>
-          <strong>Jacob Thornton</strong>
-          <small>@fat - San Francisco</small>
+      <li className="list-group-item">
+        <div className="media w-100">
+          {/* <img className="media-object rounded-circle mr-3" src="https://fillmurray.com/301/300" /> */}
+          <div className="media-body align-self-center">
+            {/* <button className="btn btn-outline-primary btn-sm float-right">
+              <span className="icon icon-add-user"></span> Follow
+                  </button> */}
+            <Link to={`/profile/${this.props.info.username}`}>
+              <strong>@{this.props.info.username}</strong>
+            </Link>
+          </div>
         </div>
-      </div>
+      </li>
     )
   }
 }
--- application/client/src/components/user/profile/following/Following.js
+++ application/client/src/components/user/profile/following/Following.js
@@ -1,64 +1,56 @@
 import React, { Component } from 'react'
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import Spinner from "../../../common/Spinner"
+import FollowCard from "./FollowCard"
 
 class Following extends Component {
   render() {
-    return (
+    const loaded = (
       <div className="text-center mt-5">
         <div className="row">
           <div className="col-md-3 col-sm-12 ml-auto mt-5">
             <h3>Followers</h3>
             <ul className="media-list media-list-users list-group">
-              <li className="list-group-item">
-                <div className="media w-100">
-                  <img className="media-object rounded-circle mr-3" src="https://fillmurray.com/301/300" />
-                  <div className="media-body align-self-center">
-                    <button className="btn btn-outline-primary btn-sm float-right">
-                      <span className="icon icon-add-user"></span> Follow
-                  </button>
-                    <strong>Jacob Thornton</strong>
-                    <small>@fat - San Francisco</small>
-                  </div>
-                </div>
-              </li>
-
+              {
+                this.props.user.info.followers && this.props.user.info.followers.length > 0 ?
+                  this.props.user.info.followers.map(info => {
+                    return <FollowCard key={info._id} info={info} />
+                  })
+                  : <p>Nobody yet</p>
+              }
             </ul>
           </div>
           <div className="col-md-3 col-sm-12 mr-auto mt-5">
             <h3>Following</h3>
             <ul className="media-list media-list-users list-group">
-              <li className="list-group-item">
-                <div className="media w-100">
-                  <img className="media-object rounded-circle mr-3" src="https://fillmurray.com/301/300" />
-                  <div className="media-body align-self-center">
-                    <button className="btn btn-outline-primary btn-sm float-right">
-                      <span className="icon icon-add-user"></span> Follow
-                  </button>
-                    <strong>Jacob Thornton</strong>
-                    <small>@fat - San Francisco</small>
-                  </div>
-                </div>
-              </li>
-              <li className="list-group-item">
-                <div className="media w-100">
-                  <img className="media-object rounded-circle mr-3" src="https://fillmurray.com/300/302" />
-                  <div className="media-body align-self-center">
-                    <button className="btn btn-outline-primary btn-sm float-right">
-                      <span className="icon icon-add-user"></span> Follow
-                  </button>
-                    <strong>Dave Gamache</strong>
-                    <small>@dhg - Palo Alto</small>
-                  </div>
-                </div>
-              </li>
-
+              {
+                this.props.user.info.following && this.props.user.info.following.length > 0 ?
+                  this.props.user.info.following.map(info => {
+                    return <FollowCard key={info._id} info={info} />
+                  })
+                  : <p>Nobody yet</p>
+              }
             </ul>
           </div>
         </div>
       </div>
     )
+
+    return (
+
+      <React.Fragment>
+        {
+          this.props.user.loading ? <Spinner /> : loaded
+        }
+      </React.Fragment>
+    )
   }
 }
 
+const mapStateToProps = state => ({
+  auth: state.auth,
+  user: state.user
+})
 
-
-export default Following;
+export default connect(mapStateToProps)(Following);
