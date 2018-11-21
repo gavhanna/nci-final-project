19f1655485139011398d6bf92c496ba39fa57f09
Gav Hanna <gavhanna@gmail.com>
2018-11-21 16:05:51 +0000

    added gifs to landing page

--- application/client/src/components/layout/Landing.js
+++ application/client/src/components/layout/Landing.js
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import { PropTypes } from "prop-types";
 import { connect } from "react-redux";
 import logo from "../common/logo.svg";
+import create from "./landingGifs/create-recipe.gif";
+import recipebook from "./landingGifs/rb-opt.gif";
+import follow from "./landingGifs/follow.gif";
+import comment from "./landingGifs/comment.gif";
 
 
 class Landing extends Component {
@@ -29,16 +33,20 @@ class Landing extends Component {
         <div className="container-fill-height bg-light landing-page-inverted">
           <div className="container-content-middle text-center">
             <div className="row pl-0 pr-0 m-0">
-              <div className="col-12 col-md-5 m-auto">
+              <div className="col-12 col-md-12 col-lg-6 m-auto">
                 <h3 className="title-font">Upload</h3>
+                <img src={create} style={{ width: "350px", maxWidth: "95vw", height: "auto" }} alt="Create Recipes" />
                 <p>Create and upload your own recipes to share with others</p>
                 <h3 className="title-font">Curate</h3>
+                <img src={recipebook} style={{ width: "350px", maxWidth: "95vw", height: "auto" }} alt="Curate a Recipe Book" />
                 <p>Add any recipes to your own personal Recipe Book</p>
               </div>
               <div className="col-12 col-md-5 m-auto">
                 <h3 className="title-font">Follow</h3>
+                <img src={follow} style={{ width: "350px", maxWidth: "95vw", height: "auto" }} alt="Follow others" />
                 <p>Follow your favourite creators and try out their latest recipes</p>
                 <h3 className="title-font">Discuss</h3>
+                <img src={comment} style={{ width: "350px", maxWidth: "95vw", height: "auto" }} alt="Follow others" />
                 <p>Talk about each recipe in the comment section</p>
               </div>
             </div>
